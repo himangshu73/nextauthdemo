deb530e9d8a3b041a1d4192c8370ec48bb13c858
himangshu <himangshu73@gmail.com>
2025-04-06 06:20:38 +0600

    home button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SignIn from "./sign-in";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="p-6 flex justify-between items-center bg-gray-200">
-      <h1 className="text-4xl cursor-pointer">Next Auth Demo</h1>
+      <Link href="/" className="text-4xl cursor-pointer">Next Auth Demo</Link>
       <div>
         <SignIn />
       </div>
