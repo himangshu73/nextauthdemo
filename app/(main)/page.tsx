620d3e0c143d079ff386ca4dc9c5ee513c0f252a
himangshu <himangshu73@gmail.com>
2025-05-21 08:29:31 +0600

    home

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,29 +1,24 @@
-"use client";
-
-import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() {
-  const { status } = useSession();
   return (
     <div className="px-6 py-4">
       <h1>Welcome to Himangshu.xyz</h1>
-      {status === "loading" ? (
-        <div>Loading...</div>
-      ) : status === "authenticated" ? (
-        <div className="flex flex-col gap-4">
-          <Link className="hover:text-red-400" href="/todolist">
-            To Do List
-          </Link>
-          <Link className="hover:text-red-400" href="/expensetracker">
-            Expense Tracker
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <Link href="/signup">SignUp</Link>
-        </div>
-      )}
+
+      <div className="flex flex-col gap-4">
+        <Link className="hover:text-red-400" href="/todolist">
+          To Do List
+        </Link>
+        <Link className="hover:text-red-400" href="/expensetracker">
+          Expense Tracker
+        </Link>
+        <Link className="hover:text-red-400" href="/blog">
+          Blog
+        </Link>
+      </div>
+      <div>
+        <Link href="/signup">SignUp</Link>
+      </div>
     </div>
   );
 }
