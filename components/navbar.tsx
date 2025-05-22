c90d2bd3df02d1539e7ae90af5e13a8a21365417
himangshu <himangshu73@gmail.com>
2025-05-22 08:39:48 +0600

    header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,24 +1,45 @@
 import Link from "next/link";
-import SignIn from "./sign-in";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
-import SignOut from "./sign-out";
 
 export default async function Navbar() {
-  const session = await getServerSession(authOptions);
-  console.log(session?.user.name);
-
   return (
-    <nav className="bg-white shadow-md p-6 py-4 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
+    <header className="bg-cyan-700 shadow-md sticky top-0 z-50">
+      <nav
+        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center"
+        aria-label="Main navigation"
+      >
         <Link
           href="/"
-          className="text-2xl font-bold text-blue-600 hover:text-blue-700 transition-colors"
+          title="Go to homepage"
+          className="text-2xl font-bold text-white"
         >
           Himangshu.xyz
         </Link>
-        {session ? <SignOut /> : <SignIn />}
-      </div>
-    </nav>
+        <ul className="flex space-x-6 text-white">
+          <li>
+            <Link
+              href="/blog"
+              title="Read our blog"
+              className="hover:font-bold"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#"
+              title="Learn more about us"
+              className="hover:font-bold"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="#" title="Contact us" className="hover:font-bold">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
