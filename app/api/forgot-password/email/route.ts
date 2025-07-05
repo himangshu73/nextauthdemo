59401fd77a5e5bb75ce1f85c2e25ab1933be9f02
himangshu <himangshu73@gmail.com>
2025-07-05 23:49:22 +0600

    password

--- app/api/forgot-password/email/route.ts
+++ app/api/forgot-password/email/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest) {
 
     const user = await UserModel.findOne({
       email,
-    });
+    }).select("+password");
     if (!user) {
       return NextResponse.json(
         {
